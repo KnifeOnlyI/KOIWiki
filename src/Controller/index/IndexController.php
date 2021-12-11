fbf11583d459c23ca426109b4cd9800f2718d85c
Dany Pignoux <dany.pignoux@outlook.fr>
2021-12-11 12:36:53 +0100

    Initial commit

---
+++ src/Controller/index/IndexController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller\index;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Represent the controller for index
+ *
+ * @author Dany Pignoux <dany.pignoux@outlook.fr>
+ */
+class IndexController extends AbstractController
+{
+    #[Route(path: '/', name: 'index')]
+    public function get(): Response
+    {
+        return $this->render('index/index.html.twig');
+    }
+}
